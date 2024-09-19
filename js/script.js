b8c4018ef94977c78abb5731495dfac78baa72f6
Vatsalwadhi <104412660+Vatsalwadhi@users.noreply.github.com>
2024-09-19 21:18:34 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let navbar = document.querySelector('.header .navbar');
+let menu = document.querySelector('#menu-btn');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let cart = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.add('active');
+}
+
+document.querySelector('#close-form').onclick = () =>{
+    cart.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    cnteredSlides:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.getElementById('menu-btn');
+    const menuSection = document.getElementById('menu');
+
+    menuBtn.addEventListener('click', () => {
+        menuSection.classList.toggle('show');
+    });
+});
